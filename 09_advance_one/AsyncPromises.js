374e15adf1bf328504db8abf44303adcf0b62071
shivam7348 <106791774+shivam7348@users.noreply.github.com>
2024-04-04 15:28:10 +0530

    Async wait wait for until work is completing

--- 09_advance_one/AsyncPromises.js
+++ 09_advance_one/AsyncPromises.js
@@ -1,63 +1,93 @@
-// const promiseOne = new Promise(function(resolve, reject){
-//     setTimeout(function(){
-//         console.log('Async is complete');
-//         resolve()
+ const promiseOne = new Promise(function(resolve, reject){
+    setTimeout(function(){
+         console.log('Async is complete');
+       resolve()
 
-//     },1000)
-// })
+     },1000)
+ })
+promiseOne.then(function(){
+     console.log("Promise consumed");
+ })
 
+//  in next method it can also be like promise
+ new Promise(function(resolve, reject){
+    setTimeout(function(){
+        console.log("Async task 2")
+      resolve()
+    },1000)
+ }).then(function(){
+    console.log("Async 2 is resolved")
+ })
 
+//  three  promises
+ const promiseThree = new Promise(function(resolve, reject ){
+    setTimeout(function(){
+        console.log("async code 3");
+        resolve({usename: "chai", email: "chaifa-border.com"})
 
-// promiseOne.then(function(){
-//     console.log("Promise consumed");
+         },1000)
+})
 
-// })
-
-// // in next method it can also be like promise
-// new Promise(function(resolve, reject){
-//     setTimeout(function(){
-//         console.log("Async task 2")
-//         resolve()
-//     },1000)
-// }).then(function(){
-//     console.log("Async 2 is resolved")
-// })
-
-// three 
-// const promiseThree = new Promise(function(resolve, reject ){
-//     setTimeout(function(){
-//         console.log("async code 3");
-//         resolve({usename: "chai", email: "chaifa-border.com"})
-
-//         },1000)
-// })
-
-// promiseThree.then(function(user){
-//     console.log(user)
-// })
+ promiseThree.then(function(user){
+     console.log(user)
+ })
 
 
    // four
 
-const fourPromise = new Promise(function(resolve, reject){
+const promiseFour = new Promise(function(resolve, reject){
     setTimeout(function(){
         let error = true
-        if (!error) {
-            resolve({username: "SHivamHitesh", password:"1234"})
+        if(!error) {
+            resolve({username: "shivam", password: "1234"})
         } else {
-            reject('Error: something went Wrong')
+            reject('ERROR: Something went wrong')         }
+
+    },1000)})
+promiseFour.then((user) => {
+    console.log(user);     
+    return user.username })
+ .then((username) => {
+     console.log(username) })
+ .catch((error) => {
+     console.log(error);
+ })
+ .finally(()=> console.log(" The promise is either resolve or reject"))
+
+
+
+ const promiseFive = new Promise(function(resolve, reject){
+     setTimeout(function(){
+         let  error = true
+        if( !error){
+             resolve({username:"javascript", password: " 12345"})
+
+         } else {
+            reject('Error: JS went wrong')
         }
+    }, 1000)
 
-    },1000)
+ })
+ 
+
+
+
+// async does wait for until working
+//  async function  consumePromiseFive(){
+//     await promiseFive
+// }
+
+
+
+
+
+
+fetch('https://api.github.com/users/shivam7348')
+.then((response) =>{
+    return response.json()
+
 })
-fourPromise.then((user) => {
-    console.log((user));
-    return user.username
-
-}).then((username)=> {
-    console.log(username);
-
-}).catch(function(error) {
-    console.log("error");
-
+.then((data) => {
+    console.log(data);
 })
+.catch((error)=> console.log(error))
