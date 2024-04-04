4b2f7595c3791f98c4ff6fe7ce9944fc3a104ecd
shivam7348 <106791774+shivam7348@users.noreply.github.com>
2024-04-04 22:26:43 +0530

    try means it is coreect value  catch() it is the error value

--- Asyncwaitproblem.js
+++ Asyncwaitproblem.js
@@ -1,21 +1,33 @@
 const fivePromise = new Promise(function(resolve){
     setTimeout(function(){
-        let error = false
+        let error = true
         if(!error){
             resolve({username: "JavaScript Error", password: "1234"})
 
         } else {
             reject('ERROR: Js went wrong')
-
         }
     }, 1000)
 })
-// if error are in the code the it should be try catch()
+
 async function consumePromiseFive() {
-     const response = await  fivePromise
-     console.log(response);
+     try{
+        const response = await fivePromise
+        console.log(response);
 
-
+     } catch(error){
+        console.log(error);
+     }
 }
-
 consumePromiseFive()
+
+
+// if error are in the code the it should be try catch()
+// async function consumePromiseFive() {
+//      const response = await  fivePromise
+//      console.log(response);
+
+
+// }
+
+// consumePromiseFive()
