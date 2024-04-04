8da642aa44dcef6640ab6787bac40791f4c8aa62
shivam7348 <106791774+shivam7348@users.noreply.github.com>
2024-04-05 00:17:33 +0530

    with the help of fetch() catch () method

--- Asyncwaitproblem.js
+++ Asyncwaitproblem.js
@@ -1,25 +1,25 @@
-const fivePromise = new Promise(function(resolve){
-    setTimeout(function(){
-        let error = true
-        if(!error){
-            resolve({username: "JavaScript Error", password: "1234"})
+// const fivePromise = new Promise(function(resolve){
+//     setTimeout(function(){
+//         let error = false
+//         if(!error){
+//             resolve({username: "JavaScript Error", password: "1234"})
 
-        } else {
-            reject('ERROR: Js went wrong')
-        }
-    }, 1000)
-})
+//         } else {
+//             reject('ERROR: Js went wrong')
+//         }
+//     }, 1000)
+// })
 
-async function consumePromiseFive() {
-     try{
-        const response = await fivePromise
-        console.log(response);
+// async function consumePromiseFive() {
+//      try{
+//         const response = await fivePromise
+//         console.log(response);
 
-     } catch(error){
-        console.log(error);
-     }
-}
-consumePromiseFive()
+//      } catch(error){
+//         console.log(error);
+//      }
+// }
+// consumePromiseFive()
 
 
 // if error are in the code the it should be try catch()
@@ -31,3 +31,37 @@ consumePromiseFive()
 // }
 
 // consumePromiseFive()
+
+// async awit method  
+
+// fetch() is a network request it wil take a time so we await for that
+
+// async function getAllUsers() {
+//     try{
+//             const response =  await fetch('https://api.github.com/users/shivam7348')
+//             console.log(response)
+//         //  const data = response.json()
+//         //     console.log(data);
+//     } catch(error) {
+//         console.log("E:", error)
+//     }
+
+ 
+// }
+// getAllUsers()
+
+fetch('https://api.github.com/users/shivam7348')
+
+.then((response) => {
+    return response.json()
+.then((data) => {
+    console.log(data)
+})
+
+}).catch((error) => console.log(error));
+
+
+
+
+
+
