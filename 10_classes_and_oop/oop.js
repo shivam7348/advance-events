e4b98ac6a54482178a53e47166a7d6afea618278
shivam7348 <106791774+shivam7348@users.noreply.github.com>
2024-04-05 08:05:20 +0530

    this is current context and constructor function()

--- 10_classes_and_oop/oop.js
+++ 10_classes_and_oop/oop.js
@@ -2,7 +2,19 @@
 const user = {
     username: " shivam",
     loginCount: 8,
-    signedIT: true
+    signedIT: true,
+    getUserDetails: function() {
+        // console.log("User")
+
+        // console.log(`Username: ${this.username} `)
+        console.log(this)
+    }
 }
+// console.log(user.getUserDetails())
 console.log(user.username)
+// console.log(this)
+
+
+
+
 
